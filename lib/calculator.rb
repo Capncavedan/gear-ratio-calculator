8a748f9add81def580955706a4602ba5942f1809
Dan Buettner <dan@danbuettner.net>
2013-11-07 21:29:05 -0600

    update tire sizes, add specs

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -10,8 +10,7 @@ class Calculator
     chainring_sizes.each do |chainring_size|
       ret2 = []
       cassette_cog_sizes.each do |cog_size|
-        ratio = wheel_radius_mm / crank_length_in_mm * chainring_size / cog_size
-        ret2 << (ratio * 100).to_i/100.0
+        ret2 << gain_ratio(chainring_size, cog_size)
       end
       ret << ret2
     end
@@ -21,15 +20,32 @@ class Calculator
 
   private
 
+
+  def gain_ratio(chainring_size, cog_size)
+    ratio = wheel_radius_mm / crank_length_in_mm * chainring_size / cog_size
+    (ratio * 100).to_i/100.0
+  end
+
   def wheel_circumference_mm
     {
-      "700x28c" => 2159,
-      "700x35c" => 2205
+      "700x20c" => 2086,
+      "700x23c" => 2097,
+      "700x25c" => 2105,
+      "700x28c" => 2136,
+      "700x32c" => 2155,
+      "700x35c" => 2168,
+      "700x38c" => 2180,
+      "700x40c" => nil,
+      "700x44c" => 2224,
+      "700x50c" => 2293,
+      "700x56c" => 2325,
+
+      "29x2.3"  => 2325
     }[tire_size] || 0
   end
 
   def wheel_radius_mm
-    wheel_circumference_mm / 3.14159 / 2.0
+    wheel_circumference_mm / 3.1415926536 / 2.0
   end
 
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,26 +2,47 @@ require 'spec_helper'
 
 describe Calculator do
 
-  it "calculates a set of gain ratios for a 1x10" do
+  it "calculates a set of gain ratios for a 1x10 with 175 crankarms" do
     s = Calculator.new
     s.chainring_sizes    = [40]
     s.cassette_cog_sizes = [11, 13, 15, 17, 19, 21, 24, 28, 32, 36]
     s.crank_length_in_mm = 175
     s.tire_size           = "700x35c"
 
-    s.gain_ratios.should eql [[7.29, 6.17, 5.34, 4.71, 4.22, 3.81, 3.34, 2.86, 2.50, 2.22]]
+    s.gain_ratios.should eql [[7.16, 6.06, 5.25, 4.63, 4.15, 3.75, 3.28, 2.81, 2.46, 2.19]]
   end
 
-  it "calculates a set of gain ratios for a 21 speed triple" do
+  it "calculates a set of gain ratios for a 20 speed double with 190 crankarms" do
+    s = Calculator.new
+    s.chainring_sizes    = [50, 34]
+    s.cassette_cog_sizes = [12, 13, 14, 15, 16, 17, 19, 21, 24, 27]
+    s.crank_length_in_mm = 190
+    s.tire_size           = "700x25c"
+
+    s.gain_ratios.should eql [[7.34, 6.78, 6.29, 5.87, 5.51, 5.18, 4.64, 4.19, 3.67, 3.26],
+                              [4.99, 4.61, 4.28, 3.99, 3.74, 3.52, 3.15, 2.85, 2.49, 2.22]]
+  end
+
+  it "calculates a set of gain ratios for a 21 speed triple with 175 crankarms" do
     s = Calculator.new
     s.chainring_sizes    = [52, 42, 30]
     s.cassette_cog_sizes = [11, 12, 13, 14, 15, 17, 19]
     s.crank_length_in_mm = 175
     s.tire_size           = "700x28c"
 
-    s.gain_ratios.should eql [[9.28, 8.5, 7.85, 7.29, 6.8, 6.0, 5.37],
-                              [7.49, 6.87, 6.34, 5.89, 5.49, 4.85, 4.34],
-                              [5.35, 4.9, 4.53, 4.2, 3.92, 3.46, 3.1]]
+    s.gain_ratios.should eql [[9.18, 8.41, 7.77, 7.21, 6.73, 5.94, 5.31],
+                              [7.41, 6.79, 6.27, 5.82, 5.43, 4.79, 4.29],
+                              [5.29, 4.85, 4.48, 4.16, 3.88, 3.42, 3.06]]
+  end
+
+  it "calculates a set of gain ratios for a singlespeed 29er with 185mm crankarms" do
+    s = Calculator.new
+    s.chainring_sizes    = [33]
+    s.cassette_cog_sizes = [16]
+    s.crank_length_in_mm = 185
+    s.tire_size           = "29x2.3"
+
+    s.gain_ratios.should eql [ [ 4.12 ] ]
   end
 
 end
